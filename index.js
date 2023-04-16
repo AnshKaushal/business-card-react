9e5e9e6aab4a0221cb63f47d70530725178a02cd
Ansh Kaushal <ansh1043.be21@chitkarauniversity.edu.in>
2023-04-16 14:08:32 +0530

    new files

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import Info from "./components/Info"
-import About from "./components/About"
-import Footer from "./components/Footer"
+const express = require("express");
+const bodyParser = require("body-parser");
+const _ = require("lodash");
+const path = require("path");
+const app = express();
 
-export default function App(){
-    return (
-        <div className="app">
-        <Info/>
-        <About/>
-        <Footer/>
-        </div>
-    )
-}
+app.set("view engine", "ejs");
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Open http://localhost:3000/");
+});
